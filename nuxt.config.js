9321a79ae58171ed05708fe70f615c555dc822f7
Alex Lavrov <alex.l@viomedia.ru>
2022-01-31 18:48:37 +0300

    update yml

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  target: 'server',
+  target: 'statis',
   router: {
     base: '/notes/'
   },
